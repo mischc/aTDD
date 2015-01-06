273d942932195399b509331b7d01149d07bb4336
mischc <mischc@gmail.com>
2015-01-06 17:25:47 -0600

    Chapter 5 - Start at Chapter 6

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -41,10 +41,6 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-#table = self.browser.find_element_by_id('id_list_table')
-#rows = table.find_elements_by_tag_name('tr')
-#self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -53,8 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table ('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table ('1: Buy peacock feathers')
         
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
