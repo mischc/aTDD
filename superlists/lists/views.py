3c1b5fa8777d4c3bcd183c40e3d8da8cade2cc15
mischc <mischc@gmail.com>
2015-01-19 16:40:43 -0600

    Chapter 6 - Start at "A Separate Template for Viewing Lists"

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
